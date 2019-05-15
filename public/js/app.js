575ff0ae7c775449edc45d1034b0d2d50b3dbb34
Sara Wegmann <sarawegmann@Saras-MacBook-Air.local>
2019-05-15 19:10:48 -0400

    created get and create routes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,37 @@
 const app = angular.module('ContactsApp', []);
 
-app.controller('contactsController', ['$http',function ($http) {
-  this.contact = "Sara and Pooja";
+app.controller('contactsController', ['$http', function ($http) {
+  const controller = this;
+  this.indexOfShowContact = null;
+
+  this.createContact = function() {
+    $http({
+      method: 'POST',
+      url: '/contacts',
+      data: {
+        name: this.name,
+        address: this.address,
+        phone: this.phone,
+        email: this.email
+      }
+    }).then(function(response) {
+      this.getContacts();
+    }, function() {
+      console.log('error');
+    })
+  }
+
+  this.getContacts = function() {
+    $http({
+      method: 'GET',
+      url: '/contacts',
+    }).then(function(response) {
+      controller.contacts = response.data;
+    }, function() {
+      console.log('error');
+    })
+  }
+
+  this.getContacts();
+
 }]);
