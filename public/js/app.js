b9ee73f5dfb4a733168b237966670c838a4aa327
Pooja Manuja <poojamanuja@Poojas-MBP.attlocal.net>
2019-05-15 20:51:20 -0400

    added edit and upadte functionality

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,42 @@ const app = angular.module('ContactsApp', []);
 app.controller('contactsController', ['$http', function ($http) {
   // const controller = this;
   this.indexOfShowContact = null;
+  this.indexOfEditFormToShow = null;
   this.createNew = {}
   this.contact = '';
   this.contacts = []
+
+  this.deleteContact = function (id) {
+      console.log('delete me ' + id);
+      $http({
+          method:'DELETE',
+          url: '/contacts/' + id
+      }).then(response => {
+        console.log(response.data);
+        const removeByIndex = this.contacts.findIndex(contact => contact._id === id)
+        this.contacts.splice(removeByIndex, 1);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  this.updateContact = function(contact) {
+    console.log("update me" + contact);
+      $http({
+        method: 'PUT',
+        url: '/contacts/' + contact._id,
+        data: {
+          address: this.updatedAddress,
+          phone: this.updatedPhone,
+          email: this.updatedEmail
+        }
+      }).then(response => {
+        console.log(response.data);
+
+      }, error => {
+        console.log(error.message)
+      })
+    }
 
   this.createContact = function() {
     $http({
